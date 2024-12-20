5c1b4cb5ee87bf9c613b47e495e06172591fc697
minhthieu213 <minhthieu213@gmail.com>
2024-12-20 10:44:01 +0700

    Init code config Router and  layout when loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout  from '~/components/Layout/DefaultLayout';
+
+import { Fragment } from 'react';
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index)=> {
+                        const Page = route.component;
+                        let Layout = DefaultLayout
+                        if(route.layout){
+                            Layout = route.layout;
+                        }else if(route.layout === null){
+                            Layout = Fragment;
+                        }
+                        return <Route key={index} 
+                                path ={route.path} 
+                                element={<Layout><Page/></Layout>}
+                                />
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
